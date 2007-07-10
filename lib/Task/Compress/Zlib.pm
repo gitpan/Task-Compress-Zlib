7b21aae0a058bde062a38ad356796fba17770743
Kevin William Henwood <ski@cpan.org>
2007-07-10 11:11:49 +0000

    Import of SKI/Task-Compress-Zlib-0.01 from CPAN.
    gitpan-cpan-distribution: Task-Compress-Zlib
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SKI/Task-Compress-Zlib-0.01.tar.gz
    gitpan-cpan-author:       SKI
    gitpan-cpan-maturity:     released

---
+++ lib/Task/Compress/Zlib.pm
@@ -0,0 +1,42 @@
+package Task::Compress::Zlib;
+
+use strict;
+use vars qw($VERSION);
+BEGIN {
+          $VERSION = '0.01';
+}          
+
+1;
+__END__
+
+=pod
+
+=head1 NAME
+
+Task::Compress::Zlib - Installs everything needed for Compress::Zlib
+
+=head1 SYNOPSIS
+
+  cpan Task::Compress::Zlib
+  
+=head1 DESCRIPTION
+  
+  This will install all the modules necessary for a proper install
+  of Compress::Zlib. See also: Bundle::Compress.
+
+=head1 AUTHOR
+
+Kevin W. Henwood, <Khen1950fx@aol.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2007 by Kevin W. Henwood
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
+
+1;
